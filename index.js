4efa8a0348a2f1229d5e6a92c6eca5f4e2c9c00d
KellyMark <kmark602@gmail.com>
2022-12-12 12:19:11 -0500

    commit tree 7945be5da2b337e5cb25b71b1681ec1c0648b098

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const num3 = 15;
+const num4 = 11;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 2 + 1);
+
+const mod = num3 % num4;
+
+const max = Math.max(1, 4, 6, 8, 20);
+
